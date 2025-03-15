4085f1cf39f422751fca808c53d0f3dfd040daee
NOZAKI Shuntaro <shun_harutaro@protonmail.com>
2025-03-15 22:23:06 +0900

    ui_v2 (#33)

--- src/screens/screen_standby.cpp
+++ src/screens/screen_standby.cpp
@@ -8,13 +8,15 @@ Button myButton(160, 120, 240, 100, false, "REC", offCol, onCol, TC_DATUM, 0, 0,
 
 void showStandbyScreen(const AppState &state) {
     M5.Lcd.clear();
+    M5.Lcd.fillScreen(WHITE);
+    M5.Lcd.setTextColor(BLACK, WHITE);
     showHeaderBar("RECボタンで開始します");
     M5.Lcd.setCursor(10,40);
     M5.Lcd.setTextDatum(0);
     //M5.Lcd.drawString("User: " + state.selectedUser, 10, 40);
     M5.Lcd.drawString("利用者: 介護士A", 10, 40);
     //myButton.draw();
-    M5.Lcd.drawRect(100, 80, 120, 80, WHITE);
+    M5.Lcd.drawRect(100, 80, 120, 80, BLACK);
     M5.Lcd.setTextDatum(4);
     M5.Lcd.drawString("REC", 160, 120);
 

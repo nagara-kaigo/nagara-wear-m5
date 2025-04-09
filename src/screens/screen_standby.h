8b43e460d52d740b286a0983ae5ca341e4ee4cc9
okota <krknkiki@gmail.com>
2025-04-09 23:38:54 +0900

    RECORD_STARTX等defineし、switch caseでレコードタイプごとに表示分けした。その画面はRECボタンにした。

--- src/screens/screen_standby.h
+++ src/screens/screen_standby.h
@@ -7,11 +7,15 @@
 #include "../ui/header.h"
 #include "../ui/footer.h"
 
-#define RECTWIDTH 80     // 幅
-#define RECTHEIGHT 80     // 高さ
+#define MEALTIME_RECTWIDTH 80     // 幅
+#define MEALTIME_RECTHEIGHT 80     // 高さ
 #define GAP 10     // 四角同士の間隔
-#define STARTX 40     // 一番左の四角の描画開始X座標
-#define STARTY 80     // 四角の描画開始Y座標
+#define MEALTIME_STARTX 40     // 一番左の四角の描画開始X座標
+#define MEALTIME_STARTY 80     // 四角の描画開始Y座標
+#define RECORD_RECTWIDTH  120
+#define RECORD_RECTHEIGHT  80
+#define RECORD_STARTX  100
+#define RECORD_STARTY  80
 
 void showStandbyScreen(const AppState &state);
 bool handleRecBtnTouch(const TouchPoint_t &touch, AppState &state);

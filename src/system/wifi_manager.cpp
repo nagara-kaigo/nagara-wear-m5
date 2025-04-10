aab08161d01141e73a3096f081189231fc2dc703
okota7412 <115968626+okota7412@users.noreply.github.com>
2025-04-11 01:26:16 +0900

    WifiとSDカードの確認をロード画面に表示させるようにした (#101)
    * 最初のAPIたたいてる時の画面を設定
    
    * 関数呼び出しの位置の修正
    
    * 最初のロード画面でSDカードとWifiのチェックを画面に表示させるようにしました

--- src/system/wifi_manager.cpp
+++ src/system/wifi_manager.cpp
@@ -1,14 +1,19 @@
 #include "wifi_manager.h"
 #include <WiFi.h>
 #include "config.h"
+#include <M5Core2.h>
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 
 void connectToWiFi() {
     WiFi.begin(ssid, password);
+    M5.Lcd.fillRect(0, 140, 340, 120, WHITE);
+    M5.Lcd.drawString("ネットワーク通信中", M5.Lcd.width() / 2, M5.Lcd.height() * 3 / 4);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
     }
+    M5.Lcd.fillRect(0, 140, 340, 120, WHITE);
     Serial.println("WiFi Connected!");
+    M5.Lcd.drawString("ネットワーク通信成功", M5.Lcd.width() / 2, M5.Lcd.height() * 3 / 4);
 }

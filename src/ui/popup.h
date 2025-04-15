a21d764d2f3f4071194980e4227b4de2df90cedd
okota <krknkiki@gmail.com>
2025-04-15 23:30:01 +0900

    レビューに従って修正

--- src/ui/popup.h
+++ src/ui/popup.h
@@ -8,6 +8,6 @@ struct AppState; // 前方宣言
 #define WIFI_POPUP_STARTX  30
 #define WIFI_POPUP_STARTY  80
 
-void showWifiPopup(const AppState &state);
+void showWifiDisconectionPopup(const AppState &state);
 
 #endif
